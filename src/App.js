f563c0a791ad83978c0c7b8ed55007d0aa157fc0
Suresh Maurya <sureshmaurya48@gmail.com>
2023-03-09 18:19:37 +0530

    ddded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-import TodoList from './components/TodoList';
+import Avatar from './components/Avatar';
+import { useEffect, useState } from 'react';
+import {Row,Col} from 'antd'
+import axios from 'axios';
 function App() {
+  const [avatar,setAvatar]=useState([]);
+  const deleteAvatar=id=>{
+    setAvatar((prevAvatar)=>prevAvatar.filter(data=>data._id!==id));
+  }
+  const updateAvatar=(id,newAvatar)=>{
+    setAvatar(prevAvatar=>prevAvatar.map(val=>val._id===id?newAvatar:val))
+  }
+  useEffect(()=>{
+axios.get('http://localhost:9000/avatar').then(data=>setAvatar(data.data))
+  },[])
+// console.log(avatar)
+
   return (
     <div className="App">
-    <TodoList/>
+<Row>
+{avatar.map(val=><Col xl={6} key={val._id}><Avatar avatar={val} deleteAvatar={deleteAvatar} updateAvatar={updateAvatar} /></Col>)}
+</Row>
     </div>
   );
 }
